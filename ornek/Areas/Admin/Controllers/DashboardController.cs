ccb28a2188c70b63c1cbb16ca5aeb7821402a367
duaalkaraan <duaalkaraan@gmail.com>
2026-06-22 13:49:45 +0300

    .

--- ornek/Areas/Admin/Controllers/DashboardController.cs
+++ ornek/Areas/Admin/Controllers/DashboardController.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ornek.Services;
 
 namespace ornek.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class DashboardController : Controller
     {
+        private readonly NewsService _newsService;
+        private readonly CategoryService _categoryService;
+
+        public DashboardController(NewsService newsService, CategoryService categoryService)
+        {
+            _newsService = newsService;
+            _categoryService = categoryService;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.NewsCount = _newsService.GetAllNews().Count;
+            ViewBag.CategoryCount = _categoryService.GetAllCategories().Count;
             return View();
         }
     }
